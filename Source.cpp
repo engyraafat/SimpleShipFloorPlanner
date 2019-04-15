46c82521c5be1967832f4d21b459fc3abda064b2
engyraafat <38763798+engyraafat@users.noreply.github.com>
2019-04-15 16:42:47 +0200

    Add files via upload
    parser works for hard macros, soft macros and pins. die parameter calculations added, width calculation in hard macros part added, input file example added

--- Source.cpp
+++ Source.cpp
@@ -3,11 +3,20 @@
 #include<vector>
 #include<istream>
 #include<unordered_map>
+#include<math.h>
+#include <algorithm>
+//#include "lp_lib.h"
 using namespace std;
 #include<fstream>
-struct HWpair {
+struct HardR {
+	string name;
 	int height;
 	int width;
+};
+struct SoftR {
+	string name;
+	int area;
+	
 };
 void split(string str, vector<string> & vec)
 {
@@ -26,11 +35,13 @@ void split(string str, vector<string> & vec)
 	}
 	vec.push_back(word);
 }
-void parser(string filename, unordered_map<string, HWpair> & mod_map)
+
+void parser(string filename, unordered_map<int, HardR> & hard_map, unordered_map<int, SoftR> & soft_map , int & pin_count, int & pin_width, int & pin_height)
 {
 	string mod_text;
 	ifstream input;
-	
+	int hard_counter = 0;
+	int soft_counter = 0;
 	input.open(filename);
 
 	if (!input.is_open()) {
@@ -41,19 +52,69 @@ void parser(string filename, unordered_map<string, HWpair> & mod_map)
 		vector<string> components;
 		getline(input, mod_text);
 		split(mod_text, components);
-		HWpair temp;
-		temp.height = stoi(components.at(2));
-		temp.width = stoi(components.at(3));
-		mod_map[components.at(1)] = temp;
+		
+		if (components.at(0) == ".hard_module")
+		{
+			HardR temp;
+			temp.name = components.at(1);
+			temp.height = stoi(components.at(2));
+			temp.width = stoi(components.at(3));
+			hard_map[hard_counter] = temp;
+			hard_counter++;
+		}
+		else if (components.at(0) == ".soft_module")
+		{
+			SoftR temp;
+			temp.name = components.at(1);
+			temp.area = stoi(components.at(2));
+			soft_map[soft_counter] = temp;
+			soft_counter++;
+		}
+		else if (components.at(0) == ".pins")
+		{
+			pin_count = stoi(components.at(1));
+			pin_width = stoi(components.at(2));
+			pin_height = stoi(components.at(3));
+
+		}
+		else
+			cout << "invalid input";
 	}
+}
+void diePerimeter(int count, int width, int height, int & outer_dim, int & inner_dim)
+{
+	int rectPerside = ceil(count / 4.0);	// the number or rectangles per side rounded up
+	int max_dim = max(width, height);		//the maximim dimension to get a square shape
+	inner_dim = max_dim * rectPerside;
+	outer_dim = inner_dim + 2 * max_dim;
+	
+}
+void HardRectangles(unordered_map < int, HardR> mp) {
+	int area = 0;
+	float W;
+	for (auto it = mp.begin(); it != mp.end(); ++it)
+	{
+		int h = it->second.height;
+		int w = it->second.width;
+		area += (h*w);
+	}
+	W = sqrt(area);
 }
 int main()
 {
 	string file_name = "modules.txt";
-	unordered_map < string, HWpair> module_map;
-	parser(file_name, module_map);
-	for (auto it = module_map.begin(); it != module_map.end(); it++)
-		cout <<  it->first << "," << it->second.height << "," << it->second.width << endl;
+	unordered_map < int, HardR> HM;
+	unordered_map < int, SoftR> SM;
+	int pin_count = 0, pin_height = 0, pin_width = 0, inner_dim = 0, outer_dim = 0;
+	parser(file_name, HM, SM ,pin_count , pin_width , pin_height );
+	for (auto it = HM.begin(); it != HM.end(); it++)
+		cout <<  it->first << "," << it->second.name << "," << it->second.height << "," << it->second.width << endl;
+	for (auto it = SM.begin(); it != SM.end(); it++)
+		cout << it->first << "," << it->second.name <<","<< it->second.area << endl;
+	cout << "pins:" << pin_count << "," << pin_height << "," << pin_width<<endl;
+	HardRectangles(HM);
+	diePerimeter(pin_count, pin_width, pin_height, outer_dim, inner_dim);
+	cout << outer_dim << "," << inner_dim;
 	system("pause");
 	return 0;
 }
