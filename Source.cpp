20e1d8353bd3d996715e762bab8a37d1bb5ce68f
engyraafat <38763798+engyraafat@users.noreply.github.com>
2019-04-20 23:47:25 +0200

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -13,8 +13,7 @@
 
 
 
-//#include "lp_lib.h"
-//#include <stdio.h>
+
 using namespace std;
 struct HardR {
 	string name;
@@ -121,20 +120,6 @@ void HardRectangles(unordered_map < int, HardR> hmp , unordered_map < int, SoftR
 	}
 	cout << "area:" << area << "sqrt" << sqrt(area);
 	initialW = sqrt(area);
-	/*int pps = (pin_count / 4);
-	if ( (initialW / pps) >= (min (pin_height, pin_width) ) )
-		core_const = 1;
-	else
-		die_cosnt = 1;
-
-	if (die_cosnt)
-		W = 4 * min(pin_height, pin_width);
-	else
-		W = initialW;
-
-	cout << "w:" << W;
-	*/
-	//W = initialW;
 	W = sqrt(area);
 	cout << "W:" << W;
 
@@ -142,37 +127,13 @@ void HardRectangles(unordered_map < int, HardR> hmp , unordered_map < int, SoftR
 	
 		IloModel model(env);
 		IloNumVarArray vars(env);
-		//IloNumVarArray x(env);
-		//IloNumVarArray y(env);
-		//IloNumVarArray z(env);
-		//IloNumVarArray xc(env);
-		//IloNumVarArray yc(env);
-		//IloNumVarArray res(env);
 		IloNumVar a(env);
-		//res.add(IloNumVar(env));
 		vars.add(IloNumVar(env));
 		model.add(IloMinimize(env, vars[0]));
 		IloRangeArray c;
 		for (int i = 1; i <= hard_count; i++)
 		{
-			//x.add(IloNumVar(env, 0, 40));
-			//x.add(IloNumVar(env));
-
-			//y.add(IloNumVar(env, 0, 40));
-			//y.add(IloNumVar(env));
-			//z.add(IloNumVar(env));
-			/*vars.add(IloNumVar(env));
-			vars.add(IloNumVar(env));
-			vars.add(IloNumVar(env));
-			model.add(vars[i] >= 0);
-			myfile << "x" << i << " >= 0" << endl;
-			model.add(vars[i+1] >= 0);
-			myfile << "y" << i << " >=0" << endl;
-			model.add(vars[i] + vars[i+2] * (hmp[i-1].height) + (1 - vars[i+2])*(hmp[i-1].width) <= W);
-			myfile << "x" << i << " + "<< hmp[i-1].height << " z"<< i << " + " << hmp[i-1].width <<" - " << hmp[i-1].width << " z" << i << " <= " << W<<endl ;
-			model.add(vars[i+1] + vars[i+2] * (hmp[i-1].width) + (1 - vars[i+2])*(hmp[i-1].height) <= vars[0]);
-			myfile << "y" << i << " + " << hmp[i - 1].width << " z" << i << " + " << hmp[i - 1].height << " - " << hmp[i - 1].height << " z" << i << " <= " << "Y" << endl;
-			cout << i << endl;*/
+			
 			vars.add(IloNumVar(env));
 			vars.add(IloNumVar(env));
 			model.add(vars[i] >= 0);
@@ -186,20 +147,14 @@ void HardRectangles(unordered_map < int, HardR> hmp , unordered_map < int, SoftR
 			cout << i << endl; 
 		}
 		int ct = hard_count;
-		/*for (auto it1 = hmp.begin(); it1 != hmp.end(); ++it1)
-			for (auto it2 = hmp.begin(); it2 != hmp.end(); ++it2)*/
+
 		for (int i = 1; i <= hard_count - 1; i++)
 			for (int j = i+1; j <= hard_count; j++)
 			{
 				vars.add(IloNumVar(env));
 				vars.add(IloNumVar(env));
-				//xc.add(IloNumVar(env));
-				//yc.add(IloNumVar(env));
-				/*model.add(x[i] + z[i] * (hmp[i].height) + (1 - z[i])*(hmp[i].width) <= x[j] + W * (xc[ct] + yc[ct]));
-				model.add(y[i] + z[i] * (hmp[i].width) + (1 - z[i])*(hmp[i].height) <= y[j] + W * (1 + xc[ct] - yc[ct]));
-				model.add(x[j] + z[j] * (hmp[j].height) + (1 - z[j])*(hmp[j].width) <= x[i] + W * (1 - xc[ct] + yc[ct]));
-				model.add(y[j] + z[j] * (hmp[j].width) + (1 - z[j])*(hmp[j].height) <= y[i] + W * (2 - xc[ct] - yc[ct]));*/
-			/*	model.add(vars[i] +vars[i+2] * (hmp[i].height) + (1 - vars[i+2])*(hmp[i].width) <= vars[j] + W * (vars[ct] + vars[ct+1]));
+			
+				model.add(vars[i] +vars[i+2] * (hmp[i].height) + (1 - vars[i+2])*(hmp[i].width) <= vars[j] + W * (vars[ct] + vars[ct+1]));
 				myfile << "x" << i << " + " << hmp[i - 1].height << " z" << i << " + " << hmp[i - 1].width << " - " << hmp[i - 1].width << " z" << i << " <= " << "x" <<j << " + " << W << " x" <<i<<j << " + " << W << " y" << i << j << endl;
 				model.add(vars[i+1] + vars[i+2] * (hmp[i].width) + (1 - vars[i+2])*(hmp[i].height) <= vars[j+1] + W * (1 + vars[ct] - vars[ct+1]));
 				myfile << "y" << i << " + " << hmp[i - 1].width << " z" << i << " + " << hmp[i - 1].height << " - " << hmp[i - 1].height << " z" << i << " <= " << "y" << j << " + "<< W <<" + " << W << " x" << i << j << " - "<< W << " y" << i << j << endl;
@@ -208,28 +163,16 @@ void HardRectangles(unordered_map < int, HardR> hmp , unordered_map < int, SoftR
 				model.add(vars[j+1] + vars[j+2] * (hmp[j].width) + (1 - vars[j+2])*(hmp[j].height) <= vars[i+1] + W * (2 - vars[ct] - vars[ct+1]));
 				myfile << "y" << j << " + " << hmp[j - 1].width << " z" << j << " + " << hmp[j - 1].height << " - " << hmp[j - 1].height << " z" << i << " <= " << "y" << i << " + " << "2 "<< W << " - " << W << " x" << i << j << " - " << W << " y" << i << j << endl;
 				ct++;
-*/
-				model.add(vars[i] + hmp[i-1].width <= vars[j] + W * (vars[ct] + vars[ct + 1]));
-				myfile << "x" << i << " + " << hmp[i - 1].width << " <= " << "x" << j << " + " << W << " x" << i << j << " + " << W << " y" << i << j << ";" << endl;
-				model.add(vars[i+1] + hmp[i-1].height<= vars[j + 1] + W * (1 + vars[ct] - vars[ct + 1]));
-				myfile << "y" << i << " + " << hmp[i - 1].height <<  " <= " << "y" << j << " + " << W << " + " << W << " x" << i << j << " - " << W << " y" << i << j << ";" << endl;
-				model.add(vars[j] + hmp[j-1].width <= vars[i] + W * (1 - vars[ct] + vars[ct + 1]));
-				myfile << "x" << j << " + " << hmp[j - 1].width << " <= " << "x" << i << " + " << W << " - " << W << " x" << i << j << " + " << W << " y" << i << j << ";" << endl;
-				model.add(vars[j + 1] + hmp[j-1].height<= vars[i + 1] + W * (2 - vars[ct] - vars[ct + 1]));
-				myfile << "y" << j << " + " << hmp[j - 1].height << " <= " << "y" << i << " + " << "2 " << W << " - " << W << " x" << i << j << " - " << W << " y" << i << j << ";" << endl;
-				ct++;
+
 			}
 
 		
 		IloCplex cplex(model);
 		cplex.solve();
-		//cout << " min =" << cplex.getObjValue() << endl;
 		cout << cplex.getStatus();
 		if (cplex.getStatus() == IloAlgorithm::Optimal) {
 			IloNumArray vals(env);
-			//cplex.getValues(vals, vars);
-			//env.out() <<"value: "<< vars[0];
-			//env.out() << "Values = " << vals << endl;
+			
 		}
 	
 	env.end();
@@ -239,55 +182,24 @@ void HardRectangles(unordered_map < int, HardR> hmp , unordered_map < int, SoftR
 
 int main()
 {
-	//IloEnv env;
-	//IloModel model(env);
-	//IloNumVarArray x(env);
-	//x.add(IloNumVar(env, 0, 40));
-	//x.add(IloNumVar(env)); // default : between 0 and +∞
-	//x.add(IloNumVar(env));
-	//model.add(-x[0] + x[1] + x[2] <= 20);
-	//model.add(x[0] - 3 * x[1] + x[2] <= 30);
-	//model.add(IloMaximize(env, x[0] + 2 * x[1] + 3 * x[2]));
-	//IloCplex cplex(model);
-	//cplex.solve();
-	//cout << " Max =" << cplex.getObjValue() << endl;
-	//if (cplex.getStatus() == IloAlgorithm::Optimal) {
-	//	IloNumArray vals(env);
-	//	cplex.getValues(vals, x);
-	//	env.out() << "Values = " << vals << endl;
-	//	env.out() << x;
-	//}
-	//env.end();
-	//system("pause");
-	/*string file_name = "modules.txt";
-	unordered_map < int, HardR> HM;
-	unordered_map < int, SoftR> SM;
-	int hard_count = 0, soft_count = 0;
-	int pin_count = 0, pin_height = 0, pin_width = 0, inner_dim = 0, outer_dim = 0;
-	parser(file_name, HM, SM, pin_count, pin_width, pin_height, hard_count, soft_count);
-	for (auto it = HM.begin(); it != HM.end(); it++)
-		cout << it->first << "," << it->second.name << "," << it->second.height << "," << it->second.width << endl;
-	for (auto it = SM.begin(); it != SM.end(); it++)
-		cout << it->first << "," << it->second.name << "," << it->second.area << endl;
-	cout << "pins:" << pin_count << "," << pin_height << "," << pin_width << endl;
-	HardRectangles(HM, SM, pin_count, pin_height , pin_width, hard_count , soft_count);
-	//diePerimeter(pin_count, pin_width, pin_height, outer_dim, inner_dim);
-	//cout << outer_dim << "," << inner_dim;
-	system("pause");
-	return 0;*/
-	float utilization = 0.6;
+	
+	float utilization = 0.9;
+	float suti = 0.72;
 	float aspect_min = 0.5;
-	float aspect_max = 2;
+	float aspect_max = 1/0.5;
 	int WafterU;
-	int pin_max;
+	int pin_max, pin_min;
 	int pins_side;
 	int pins_min_width;
 	int init_W;
-	string file_name = "modules.txt";
+	bool io_del = 0;
+	string file_name = "test8.txt";
 	unordered_map < int, HardR> HM;
 	unordered_map < int, SoftR> SM;
-	int area = 0;
-	int totalw = 0;
+	double area = 0;
+	float totalw = 0;
+	float totalh = 0;
+	int totalmax = 0;
 	int W;
 	int hard_count = 0, soft_count = 0;
 	int pin_count = 0, pin_height = 0, pin_width = 0, inner_dim = 0, outer_dim = 0;
@@ -301,24 +213,37 @@ int main()
 		int w = it->second.width;
 		area += (h*w);
 		totalw += w;
+		totalh += h;
 	}
 	for (auto it = SM.begin(); it != SM.end(); ++it)
 	{
-		
+		float wmin_i, wmax_i, hmin_i, hmax_i;
+		int iarea;
 		area += it->second.area;
+		iarea = it->second.area;
+		wmin_i = sqrt(iarea*aspect_min);
+		wmax_i = ceil(sqrt(iarea*aspect_max));
+		hmax_i = iarea / wmax_i + (wmax_i - wmin_i) * (iarea / (wmax_i*wmax_i));
+		hmin_i = iarea / wmax_i + (wmax_i - wmax_i) * (iarea / (wmax_i*wmax_i));
+		totalw += wmax_i;
+		totalh += hmax_i;
 	}
-	
+	totalmax = max(totalh, totalw);
 	init_W = ceil(sqrt(area));
 	WafterU = init_W / utilization;
 	pin_max = max(pin_height, pin_width);
-	pins_side = ceil(pin_count / 4);
+	pin_min = min(pin_height, pin_width);
+	pins_side = ceil(pin_count / 4.0);
 	pins_min_width = pins_side * pin_max;
+	if (pins_min_width >= WafterU)
+		io_del = 1;
 	W = max(WafterU, pins_min_width);
-	//W = totalh;
+
 	cout << "W = " << W;
+	int wid = W;
 	IloEnv env;
 	stringstream logfile;
-
+	cout << "total" << totalw << " , from area:" << wid;
 	try {
 		IloModel model(env);
 		IloNumVarArray vars(env);
@@ -327,114 +252,129 @@ int main()
 		ofstream LogFile("LogFile.txt");
 		ofstream Results("Results.txt");
 		cplex.setOut(LogFile);
+
 		// Set options of IP solver
 		cplex.setParam(IloCplex::TiLim, 100.000);
 		cplex.setParam(IloCplex::Threads, 1);
 		cplex.setParam(IloCplex::EpGap, 0.0);
 		cplex.setParam(IloCplex::EpAGap, 0.0);
-		vars.add(IloIntVar(env));
+		vars.add(IloNumVar(env));
 		IloRangeArray c(env);
 		
-		// Variables
-		for (int i = 1; i <= hard_count*2; i+=2)
+		// Hard Modules Constrains 
+		for (int i = 1; i <= hard_count*3; i+=3)
 		{
-			//cout << "ii=" << i << endl;
-			//cout << "h=" << HM[i / 2].height<<endl;
-			//cout << "w=" << HM[i / 2].width<<endl;
-			vars.add(IloIntVar(env));
-			vars.add(IloIntVar(env));
+			
+			vars.add(IloNumVar(env));
+			vars.add(IloNumVar(env));
+			vars.add(IloNumVar(env));
+
 			c.add(vars[i] >= 0);
 			c.add(vars[i + 1] >= 0);
-			c.add(vars[i] + HM[i/2].width <= W);
-			c.add(vars[i + 1] + HM[i/2].height - vars[0]<= 0);
+			//c.add(vars[i] + HM[i/2].width <= W);
+			c.add(vars[i] + HM[i / 3].width * (1- vars[i+2]) + HM[i / 3].height*vars[i+2] <= W);
+			c.add(vars[i + 1] + HM[i/3].height*(1 - vars[i + 2]) + HM[i / 3].width*vars[i + 2] - vars[0]<= 0);
+			c.add(((vars[i+2] == 1) + (vars[i+2] == 0)) == 1);
+
 		}
 		
-		int ct = hard_count*2+1;
-		for (int i = 1; i <= hard_count*2 - 2; i+=2)
-			for (int j = i + 2; j <= hard_count * 2; j += 2)
+		// Hard Modules Overlapping Constrains 
+		int ct = hard_count*3+1;
+		for (int i = 1; i <= hard_count*3 - 3; i+=3)
+			for (int j = i + 3; j <= hard_count * 3; j += 3)
 			{
-				vars.add(IloIntVar(env));
-				vars.add(IloIntVar(env));
-				//cout << "i=" << i<<endl;
-				//cout << "j=" << j<<endl;
-				c.add(vars[i] + HM[i/2].width - (vars[j] + W * (vars[ct] + vars[ct + 1]))  <= 0);
-				c.add(vars[i + 1] + HM[i/2].height - (vars[j + 1] + W * (1 + vars[ct] - vars[ct + 1])) <= 0 );
-				c.add(vars[j] + HM[j/2].width - (vars[i] + W * (1 - vars[ct] + vars[ct + 1])) <= 0 );
-				c.add(vars[j + 1] + HM[j/2].height - (vars[i + 1] + W * (2 - vars[ct] - vars[ct + 1])) <= 0 );
+				vars.add(IloNumVar(env));
+				vars.add(IloNumVar(env));
+
+			
+				c.add(vars[i] + HM[i / 3].width*(1-vars[i+2]) + HM[i / 3].height*vars[i+2] - (vars[j] + W * (vars[ct] + vars[ct + 1])) <= 0);
+				c.add(vars[i + 1] + HM[i / 3].height* (1-vars[i+2]) + HM[i / 3].width*vars[i+2] - (vars[j + 1] + W * (1 + vars[ct] - vars[ct + 1])) <= 0);
+				c.add(vars[j] + HM[j / 3].width*(1-vars[j+2]) + HM[j / 3].height*vars[j+2] - (vars[i] + W * (1 - vars[ct] + vars[ct + 1])) <= 0);
+				c.add(vars[j + 1] + HM[j / 3].height*(1-vars[j+2]) + HM[j / 3].width * vars[j+2] - (vars[i + 1] + W * (2 - vars[ct] - vars[ct + 1])) <= 0);
 				c.add(((vars[ct] == 1) + (vars[ct] == 0)) == 1);
 				c.add(((vars[ct+1] == 1) + (vars[ct+1] == 0)) == 1);
 				ct+=2;
 				cout << "ct=" << ct << endl;
 			}
 		cout << "final_ct" << ct;
-		// x -> i , y -> i+1 , w -> i+2 , delta -> i+3 , c -> i+4
+
+		// Soft Modules Cosntrains
 		for (int i = ct; i < (ct+ soft_count * 4); i += 4)
 		{
 			int iarea;
 			float wmin_i, wmax_i, hmin_i, hmax_i, deltai , ci;
-			int wmin, wmax, hmin, hmax;
 			cout << "ii=" << i << endl;
-			iarea = SM[(i - ct)/4].area;
+			iarea = SM[(i - ct)/4].area/suti;
 			wmin_i = sqrt(iarea*aspect_min);
-			wmax_i = sqrt(iarea*aspect_max);
-			hmax_i = iarea / wmax_i + (wmax_i - wmin_i) * (iarea / (wmax_i*wmax_i));
+			wmax_i = ceil(sqrt(iarea*aspect_max));
+			hmax_i = ceil(iarea / wmax_i + (wmax_i - wmin_i) * (iarea / (wmax_i*wmax_i)));
 			hmin_i = iarea / wmax_i + (wmax_i - wmax_i) * (iarea / (wmax_i*wmax_i));
-			wmin = ceil(wmin_i);
-			wmax = ceil(wmax_i);
-			hmin = ceil(hmin_i);
-			hmax = ceil(hmax_i);
-			deltai = (hmin_i - hmax_i) / (wmax_i - wmin_i);
-			ci = hmax_i - deltai * wmin_i;
-			vars.add(IloIntVar(env));
-			vars.add(IloIntVar(env));
-			vars.add(IloIntVar(env));
-			vars.add(IloIntVar(env));
+			deltai = ceil( (hmin_i - hmax_i) / (wmax_i - wmin_i));
+			ci = ceil( hmax_i - deltai * wmin_i);
+			vars.add(IloNumVar(env));
+			vars.add(IloNumVar(env));
+			vars.add(IloNumVar(env));
+			vars.add(IloNumVar(env));
 			c.add(vars[i] >= 0);
 			c.add(vars[i + 1] >= 0);
-			c.add(vars[i] + vars[i+2] <= W);
-			c.add(vars[i + 1] +vars[i+3] - vars[0] <= 0);
+			c.add(vars[i] + vars[i+2] - W <= 0 );
+			c.add(vars[i + 1] + vars[i+3] - vars[0] <= 0);
+			c.add((vars[i + 3] - vars[i + 2] * deltai - ci) == 0);
 			c.add(vars[i + 2] >= wmin_i);
 			c.add(vars[i + 2] <= wmax_i);
-			c.add(vars[i + 3] >= hmin_i);
-			c.add(vars[i + 3] <= hmax_i);
-			c.add((vars[i + 3] - vars[i + 2] * deltai - ci) == 0);
+		
 		}
+
+		// Soft Modules Non Overlapping Constrains
 		
 		int cts = ct + soft_count*4 ;
 		cout << "cts:" << cts;
 		for (int i = ct; i < (ct + soft_count*4 - 4); i += 4)
 			for (int j = i + 4; j < (ct + soft_count * 4); j += 4)
 			{
-				int iarea;
-				float wmin_i, wmax_i, hmin_i, hmax_i, deltai, ci;
-				int wmin, wmax, hmin, hmax;
-				iarea = SM[(i - ct)/4].area;
-				wmin_i = sqrt(iarea*aspect_min);
-				wmax_i = sqrt(iarea*aspect_max);
-				hmax_i = iarea / wmax_i + (wmax_i - wmin_i) * ( iarea/ (wmax_i*wmax_i) );
-				hmin_i = iarea / wmax_i + (wmax_i - wmax_i) * (iarea / (wmax_i*wmax_i));
-				deltai = (hmin_i - hmax_i) / (wmax_i - wmin_i);
-				ci = hmax_i - deltai * wmin_i;
+			
 
-				int jarea;
-				float wmin_j, wmax_j, hmin_j, hmax_j, deltaj, cj;
-				iarea = SM[(j - ct)/4].area;
-				wmin_j = sqrt(jarea*aspect_min);
-				wmax_j = sqrt(jarea*aspect_max);
-				hmax_j = jarea / wmax_j + (wmax_j - wmin_j) * (jarea / (wmax_j*wmax_j));
-				hmin_j = jarea / wmax_j + (wmax_j - wmax_j) * (iarea / (wmax_j*wmax_j));
-				
-
-
-				vars.add(IloIntVar(env));
-				vars.add(IloIntVar(env));
+				vars.add(IloNumVar(env));
+				vars.add(IloNumVar(env));
 				cout << "i=" << i << endl;
 				cout << "j=" << j << endl;
 				cout << "ct=" << ct << endl;
-				c.add(vars[i] + vars[i+2] - (vars[j] + W * (vars[cts] + vars[cts + 1])) <= 0);
-				c.add(vars[i + 1] + vars[i+3] - (vars[j + 1] + W * (1 + vars[cts] - vars[cts + 1])) <= 0);
-				c.add(vars[j] + vars[j+2] - (vars[i] + W * (1 - vars[cts] + vars[cts + 1])) <= 0);
-				c.add(vars[j + 1] + vars[j+3] - (vars[i + 1] + W * (2 - vars[cts] - vars[cts + 1])) <= 0);
+				c.add(vars[i] + vars[i+2] - (vars[j] + wid * (vars[cts] + vars[cts + 1])) <= 0);
+				c.add(vars[i + 1] + vars[i+3] - (vars[j + 1] + wid * (1 + vars[cts] - vars[cts + 1])) <= 0);
+				c.add(vars[j] + vars[j + 2] - (vars[i] + wid * ( 1 - vars[cts] + vars[cts + 1])) <= 0);
+				c.add(vars[j + 1] + vars[j+3] - (vars[i + 1] + wid * (2 - vars[cts] - vars[cts + 1])) <= 0);
+				c.add(((vars[cts] == 1) + (vars[cts] == 0)) == 1);
+				c.add(((vars[cts + 1] == 1) + (vars[cts + 1] == 0)) == 1);
+				cts += 2;
+			}
+
+		// Hard Modules with soft modules non overlapping constrains
+
+		for (int i = 1; i <= hard_count * 3; i += 3)
+
+			for (int j = ct; j < (ct + soft_count * 4); j += 4) {
+
+
+				int jarea;
+				float wmin_j, wmax_j, hmin_j, hmax_j, deltaj, cj;
+				jarea = SM[(j - ct) / 4].area / suti;
+				wmin_j = sqrt(jarea*aspect_min);
+				wmax_j = sqrt(jarea*aspect_max);
+				hmax_j = jarea / wmax_j + (wmax_j - wmin_j) * (jarea / (wmax_j*wmax_j));
+				hmin_j = jarea / wmax_j + (wmax_j - wmax_j) * (jarea / (wmax_j*wmax_j));
+				deltaj = (hmin_j - hmax_j) / (wmax_j - wmin_j);
+				cj = hmax_j - deltaj * wmin_j;
+
+
+				vars.add(IloNumVar(env));
+				vars.add(IloNumVar(env));
+				cout << "i=" << i << endl;
+				cout << "j=" << j << endl;
+				cout << "ct=" << ct << endl;
+				c.add(vars[i] + vars[i + 2] * HM[i / 3].height + (1 - vars[i + 2])*HM[i / 3].width - (vars[j] + W * (vars[cts] + vars[cts + 1]) ) <= 0 );
+				c.add( vars[i] - vars[j+2] - vars[j] + W*(1-vars[cts] + vars[cts+1]) >= 0  );
+				c.add(vars[i + 1] + vars[i + 2] * HM[i / 3].width + (1 - vars[i + 2])* HM[i / 3].height - vars[j + 1] - W * (1 + vars[cts] - vars[cts + 1]) <= 0);
+				c.add(vars[i+1] - vars[j+3] - vars[j+1] + W*(2 - vars[cts] - vars[cts + 1]) >= 0);
 				c.add(((vars[cts] == 1) + (vars[cts] == 0)) == 1);
 				c.add(((vars[cts + 1] == 1) + (vars[cts + 1] == 0)) == 1);
 				cts += 2;
@@ -445,10 +385,60 @@ int main()
 
 		// Solve
 		cplex.solve();
+
+		// Put Results in output file
 		if (cplex.getStatus() == IloAlgorithm::Optimal) {
 			IloNumArray vals(env);
 			cplex.getValues(vals, vars);
-			Results << "Values = " << vals << endl;
+			Results << "W:" << W + 2*pin_max << endl;
+			if (io_del)
+			{
+				Results << "H:" << pins_min_width + 2 * pin_max << endl;
+				Results << "Total Area:" << W * pins_min_width << endl;
+				Results << "Final Utilization:" << area / (W * pins_min_width) << endl;
+			}
+			else
+			{
+				Results << "H:" << vals[0] + 2 * pin_max << endl;
+				Results << "Total Area:" << W * vals[0] << endl;
+				Results << "Final Utilization:" << area / (W * vals[0]) << endl;
+			}
+			
+			Results << "I/O pads:" << endl;
+			for (int i = 1; i <= pins_side; i++)
+			{
+				Results << "0 , " << i * pin_min<<endl;
+			}
+
+			for (int i = 1; i <= pins_side; i++)
+			{
+				Results << i * pin_min << " , 0" << endl;
+			}
+
+			for (int i = 1; i <= pins_side; i++)
+			{
+				Results << W << " , " << i * pin_min << endl;
+			}
+			for (int i = 1; i <= pins_side; i++)
+			{
+				Results << i * pin_min << " , "<< vals[0] << endl;
+			}
+
+			Results << "Hard Modules:" << endl;
+			for (int i = 1; i <= hard_count * 3; i += 3) 
+			{
+				Results<< HM[i/3].name<<":		" << vals[i] + pin_max << "," <<vals[i+1]+pin_max<<"		";
+				if (vals[i + 2])
+					Results << "rotated";
+				Results << endl;
+			}
+
+			Results << "Soft Modules:" << endl;
+			for (int i = ct; i < (ct + soft_count * 4); i += 4)
+			{
+				Results <<SM[i/4 - hard_count - 1].name<<":		"<< vals[i]+pin_max << "		" << vals[i + 1]+pin_max <<"	" << vals[i+2] << "		" << vals[i+3]<<endl;
+			}
+			cout << "Values = " << vals << endl;
 		}
 		else
 		{
